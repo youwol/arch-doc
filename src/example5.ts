29d475042a1afd634b24b499141e9a812f17c8fc
Xaliphostes <5073694+xaliphostes@users.noreply.github.com>
2022-03-20 08:38:53 +0100

    updated example incremental remote

--- src/example5.ts
+++ src/example5.ts
@@ -6,11 +6,16 @@
  *
  * ```js
  * const arch = require('arch.node')
+ * const geom = require('@youwol/geometry')
+ * const math = require('@youwol/math')
+ * const df   = require('@youwol/dataframe')
+ * const io   = require('@youwol/io')
+ * const fs   = require('fs')
  * 
  * const model = new arch.Model()
- * model.setMaterial ( 0.25, 30e9, 2000 )
+ * model.setMaterial( 0.25, 1, 1 )
  * 
- * const fault = new arch.Surface([0,0,0, 1,0,0, 1,1,0], [0,1,2])
+ * const fault = new arch.Surface([0,0,0, 1,0,0, 1,1,-0.5], [0,1,2])
  * fault.setBC("dip",    "free", 0)
  * fault.setBC("strike", "free", 0)
  * fault.setBC("normal", "free", 0)
@@ -22,21 +27,30 @@
  * const solver = new arch.Forward(model, 'seidel', 1e-8, 200)
  * solver.setAutoReleaseMemory(false)
  * 
- * const points = new Array(9).fill(0).map( v => Math.random() )
+ * const grid = geom.generateRectangle({
+ *      a: 5, b: 5, 
+ *      na: 100, nb: 100, 
+ *      center: [0.5,0.5,2]
+ * })
+ * let points = grid.series.positions.array
  * 
  * // ------------------------------------------------
  * 
- * const n = 10
- * for (let i=0; i<n; ++i) {
- *     // Increment the remote
- *     remote.setFunction( (x,y,z) => [0,0,0,0,0, -1*i/(n-1)] )
+ * const n = 10 // nb steps
+ * remote.setFunction( (x,y,z) => [0,0,0,0,0, -1000/n] )
  * 
+ * for (let i=0; i<n; ++i) {
  *     solver.run()
  * 
  *     // Cumulate the deformation at points
- *     const d = solution.displ(points)
- *     displ = displ.map( (v,j) => v+d[j] )
+ *     const u = solution.displ(points)
+ *     points = points.map( (v,j) => v+u[j])
  * }
+ * 
+ * grid.series.positions = df.Serie.create({array: points, itemSize: 3})
+ * console.log( math.minMax(grid.series.positions) )
+ * 
+ * fs.writeFile('deformed-grid.gcd', io.encodeGocadTS(grid), 'utf8', err => {})
  * ```
 */
 export namespace Example_incremental_remote {}
